80d1435f3fe316fbc6cd172298015a455340d9ad
Miguel Angel Andrades Camacho Frontend <teoras1985@gmail.com>
2026-05-22 20:47:22 +0200

    Add password generator functionality

--- script.js
+++ script.js
@@ -2,19 +2,42 @@ const passwordLength = document.querySelector("#password-length");
 const lengthValue = document.querySelector("#length-value");
 const generateButton = document.querySelector("#generate-btn");
 const passwordOutput = document.querySelector("#password-output");
+const uppercaseCheckbox = document.querySelector("#uppercase");
+const lowercaseCheckbox = document.querySelector("#lowercase");
+const numbersCheckbox = document.querySelector("#numbers");
+const symbolsCheckbox = document.querySelector("#symbols");
+const copyButton = document.querySelector("#copy-btn");
 
 function generatePassword() {
-  const passwords = [
-    "Dragon123",
-    "Tiger456",
-    "Phoenix789",
-    "Samurai321",
-    "Ninja654"
-  ];
+  let characters = "";
 
-  const randomIndex = Math.floor(Math.random() * passwords.length);
+  if (uppercaseCheckbox.checked) {
+    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
 
-  return passwords[randomIndex];
+  if (lowercaseCheckbox.checked) {
+    characters += "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (numbersCheckbox.checked) {
+    characters += "0123456789";
+  }
+
+  if (symbolsCheckbox.checked) {
+    characters += "!@#$%^&*";
+  }
+
+  if (characters === "") {
+  return "Selecciona al menos una opción";
+}
+  let password = "";
+
+  for (let i = 0; i < passwordLength.value; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+
+  return password;
 }
 passwordLength.addEventListener("input", function () {
   lengthValue.textContent = passwordLength.value;
@@ -22,4 +45,14 @@ passwordLength.addEventListener("input", function () {
 
 generateButton.addEventListener("click", function () {
   passwordOutput.value = generatePassword();
+});
+
+copyButton.addEventListener("click", function () {
+  navigator.clipboard.writeText(passwordOutput.value);
+
+  copyButton.textContent = "✅ Copiado";
+
+  setTimeout(function() {
+  copyButton.textContent = "📋 Copiar";
+}, 2000);
 });
